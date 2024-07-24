914657817680c8babec4799ad982360c9115688c
Waleed Robertson <waleedrbrtsn1@email.com>
2024-07-24 13:47:21 -0400

    Question 2 & 3 complete; working on last problem of lab

--- server.js
+++ server.js
@@ -8,7 +8,7 @@
 import express from "express";
 // Create an Express app
 const app = express()
-const PORT = 3000; 
+const PORT = 3000;
 
 app.listen(3000, () => {
   console.log('Listening on port 3000')
@@ -34,11 +34,50 @@ app.get('/greetings/:name', (req, res) => {
 // “You rolled a 14.”
 
 app.get('/roll/:number', (req, res) => {
-
-  
   const number = req.params.number;
-  if(number != (typeof number))
-  res.send ('You must specify a number'); 
-
-else res.send(`You rolled a ${number}`); 
+ //isNaN generated with the help of chatGPT 
+  if (isNaN(`${number}`)) {
+    res.send('choose a number');} 
+  else {
+    res.send(`'You rolled ${number}'`)
+  }
 })
+
+// 3. I Want THAT One!
+// Task: Create a route for URLs like /collectibles/<index-parameter>.
+// Examples: Matches routes such as /collectibles/2 or /collectibles/0.
+const collectibles = [
+  { name: 'shiny ball', price: 5.95 },
+  { name: 'autographed picture of a dog', price: 10 },
+  { name: 'vintage 1970s yogurt SOLD AS-IS', price: 0.99 }
+];
+
+app.get('/collectibles/:index', (req, res) => {
+  const index = req.params.index;
+  if (req.params.index > 2 ) {
+res.send ('This item is not yet in stock. Check back soon!')
+  }
+
+  else {res.send(`'"So you want the ${collectibles[index].name} for ${collectibles[index].price} it's yours!" '`)};
+});
+
+// 4. Filter Shoes by Query Parameters
+// Use the following array of shoes in this challenge:
+// Task: Create a route /shoes that filters the list of shoes based on query parameters.
+// Query Parameters:
+// min-price: Excludes shoes below this price.
+// max-price: Excludes shoes above this price.
+// type: Shows only shoes of the specified type.
+// No parameters: Responds with the full list of shoes.
+
+const shoes = [
+  { name: "Birkenstocks", price: 50, type: "sandal" },
+  { name: "Air Jordans", price: 500, type: "sneaker" },
+  { name: "Air Mahomeses", price: 501, type: "sneaker" },
+  { name: "Utility Boots", price: 20, type: "boot" },
+  { name: "Velcro Sandals", price: 15, type: "sandal" },
+  { name: "Jet Boots", price: 1000, type: "boot" },
+  { name: "Fifty-Inch Heels", price: 175, type: "heel" }
+];
+
+
